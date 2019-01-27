06f498904b84010d7888c4d1a55d3955ec5443b7
smertelny <smertelny@gmail.com>
2019-01-27 09:35:18 +0200

    Add custom 404 handler; Fixed xml response tags to be lowercase

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/smertelny/ibanCheckerService
+
+require github.com/go-chi/chi v4.0.1+incompatible
--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
@@ -17,7 +18,8 @@ import (
 
 // SuccessfulResponse is used in case it is all OK
 type SuccessfulResponse struct {
-	Result string `json:"result" xml:"result"`
+	XMLName xml.Name `json:"-" xml:"response"`
+	Result  string   `json:"result" xml:"result"`
 }
 
 func (r SuccessfulResponse) String() string {
@@ -26,13 +28,15 @@ func (r SuccessfulResponse) String() string {
 
 // ErrorContent is the content of error json object
 type ErrorContent struct {
-	Msg  string `json:"message" xml:"message"`
-	Code int    `json:"status_code"  xml:"status_code"`
+	XMLName xml.Name `json:"-" xml:"error"`
+	Msg     string   `json:"message" xml:"message"`
+	Code    int      `json:"status_code"  xml:"status_code"`
 }
 
 // ErrorResponse is made for better json output
 type ErrorResponse struct {
-	Error ErrorContent `json:"error" xml:"error"`
+	XMLName xml.Name     `json:"-" xml:"errors"`
+	Error   ErrorContent `json:"error" xml:"error"`
 }
 
 func (r ErrorResponse) String() string {
@@ -40,39 +44,49 @@ func (r ErrorResponse) String() string {
 }
 
 func checkIban(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Allow", "GET, HEAD")
 	code := chi.URLParam(r, "code")
 
 	err := iban.Check(code)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		utils.Render(w, r, ErrorResponse{ErrorContent{err.Error(), http.StatusBadRequest}})
+		utils.Render(w, r, ErrorResponse{Error: ErrorContent{Msg: err.Error(), Code: http.StatusBadRequest}})
 	} else {
 		w.WriteHeader(http.StatusOK)
-		utils.Render(w, r, SuccessfulResponse{fmt.Sprintf("IBAN %v is valid", code)})
+		utils.Render(w, r, SuccessfulResponse{Result: fmt.Sprintf("IBAN %v is valid", code)})
 	}
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	w.Header().Set("Allow", "GET, HEAD")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Hello! Follow to <a href=\"/v1/iban/examplecode\">\"%v/v1/iban/YOUR_CODE_HERE\"</a> to check your iban", r.Host)
 }
 
+func custom404(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	utils.Render(w, r, ErrorResponse{Error: ErrorContent{Msg: "Not found", Code: http.StatusNotFound}})
+}
+
 func main() {
 	router := chi.NewRouter()
-
+	router.NotFound(custom404)
 	// Middlewares
 	router.Use(middleware.RequestID)
 	router.Use(middleware.URLFormat)
 	router.Use(middleware.RealIP)
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.Logger)
+	router.Use(middleware.GetHead)
 	router.Use(middleware.Timeout(time.Second * 30))
-	router.Use(md.FormatMiddleware)
-	// router.Use(middleware.AllowContentType()
+	router.Use(middleware.RedirectSlashes)
 
-	router.Get("/v1", index)
-	router.Get("/v1/iban/{code}", checkIban)
+	router.Route("/v1", func(router chi.Router) {
+		router.Use(md.FormatMiddleware)
+		router.Get("/", index)
+		router.Get("/iban/{code}", checkIban)
+	})
 
 	log.Print("Server started")
 	log.Fatal(http.ListenAndServe(":8000", router))
